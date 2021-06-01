f25e7d2fc4effefacb5adcd4a0e1235f59870e11
Amin Latifi <a.latifi.al@gmail.com>
2021-06-01 17:44:44 +0430

    upgraded some dependencies and updated tests according

--- src/views/UnsubscribeMessage.jsx
+++ src/views/UnsubscribeMessage.jsx
@@ -23,7 +23,7 @@ class UnsubscribeMessage extends React.Component {
       <html>
         <head>
           <title>Unsubscribe from Giveth notification emails</title>
-          <meta charset="utf-8"/>
+          <meta charSet="utf-8"/>
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossOrigin="anonymous"/>
